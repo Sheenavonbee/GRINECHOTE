8547ef239b3081ca83ed1064e8644ebd6a2e0ff5
ValerieBerenguer <lacorogneval@hotmail.fr>
2020-11-11 10:40:48 +0100

    responsive commit

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-let modal = document.getElementById("modal_wrapper");
 
-let open = document.getElementById("modal");
-
-let close = document.getElementById("close_modal")[0];
-
-open.onclick = function() {
-    modal.style.display = "block";
-}
-
-close.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
